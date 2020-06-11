6ee030711c18191d0cb2149892de5d192ce00eea
Shaun Gedye <sgedye88@yahoo.com.au>
2020-06-11 11:25:22 +0800

    Updated style

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,14 @@ import React from 'react'
 
 function Card(props) {
   const dragStart = e => e.dataTransfer.setData('card_id', e.target.id)
-  const dragOver = e => e.stopPropagation() // Not sure this is needed.
-
+  // const dragOver = e => e.stopPropagation() // Not sure this is needed.
   return (
     <div
       id={props.id}
       className={props.className}
       draggable={props.draggable}
       onDragStart={dragStart}
-      onDragOver={dragOver}
+      // onDragOver={dragOver}
     >
       { props.children }
     </div>
