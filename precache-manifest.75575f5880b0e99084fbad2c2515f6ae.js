07375e8525ea97b0fadfb8c250d71ffc9672f2d2
Shaun Gedye <sgedye88@yahoo.com.au>
2020-06-11 11:55:20 +0800

    Updates

---
+++ precache-manifest.75575f5880b0e99084fbad2c2515f6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3618836a79c9604bbf8967cc92c4b012",
+    "url": "/drag-n-drop/index.html"
+  },
+  {
+    "revision": "1033096e0040e195ae1c",
+    "url": "/drag-n-drop/static/css/main.e8a939f1.chunk.css"
+  },
+  {
+    "revision": "6c4ae05076275d8b7bf4",
+    "url": "/drag-n-drop/static/js/2.20cd5536.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drag-n-drop/static/js/2.20cd5536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1033096e0040e195ae1c",
+    "url": "/drag-n-drop/static/js/main.a3407812.chunk.js"
+  },
+  {
+    "revision": "8044d5f346f1caac9c56",
+    "url": "/drag-n-drop/static/js/runtime-main.d52fc905.js"
+  }
+]);
